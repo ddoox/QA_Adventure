af19aaf62dd99b9e8de91488fa73502d62549a79
ddoox <dudek746@gmail.com>
2020-06-06 11:18:33 +0200

    Test

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello Jenkins auto build");
-        System.out.println("Yay, commit spam :)");
+        System.out.println("More commits");
     }
 }
